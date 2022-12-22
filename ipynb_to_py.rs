49153072b4509a6e4312404e1c118df828f63640
Jordan Dennis <jdenn105@gmail.com>
2022-12-23 09:02:02 +1000

    Creating a simple file for protyping the program and reading it to a string successfully.

--- ipynb_to_py.rs
+++ ipynb_to_py.rs
@@ -1,12 +1,16 @@
 fn main()
 {
-    let file_name: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
  
-    if file_name.len() > 2
+    if args.len() > 2
     {
         panic!("Error: More than one argument recieved!");
     }
+
+    let file_name: &String = &args[1];
  
-    let ipynb: String = std::fs::read_to_string(&file_name[0])
+    let ipynb: String = std::fs::read_to_string(file_name)
         .expect("File not found!");
+
+	let _ipynb: File = std::io::File::open(file_name);
 }
