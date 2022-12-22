8b78e5f6c786b01432b9107801b7b3c59d659d12
Jordan Dennis <jdenn105@gmail.com>
2022-12-23 09:22:04 +1000

    So in \`Rust\` you must declare the failure the entire way through the function.

--- ipynb_to_py.rs
+++ ipynb_to_py.rs
@@ -1,22 +1,38 @@
 use std::fs::File;
-use std::io::Error;
-use std::io::BufReader;
+use std::io::{Write, BufReader, BufRead, Error};
 
-fn main()
-{
+
+// fn main() -> Result<(), Error> {
+    // let path = "lines.txt";
+// 
+    // let mut output = File::create(path)?;
+    // write!(output, "Rust is Fun")?;
+// 
+    // let input = File::open(path)?;
+    // let buffered = BufReader::new(input);
+// 
+    // for line in buffered.lines() {
+        // println!("{}", line?);
+    // }
+// 
+    // Ok(())
+// }
+
+
+fn main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
  
-    if args.len() > 2
-    {
+    if args.len() > 2 {
         panic!("Error: More than one argument recieved!");
     }
 
-    let file_name: &String = &args[1];
-	let ipynbd = File::open(file_name);
+    let file_name = &args[1];
+	let ipynb = File::open(file_name)?;
 	let buf_ipynb = BufReader::new(ipynb);
 
-	for line in buf_ipynb.lines() 
-	{
-		println!(line);
+	for line in buf_ipynb.lines() {
+		println!("{}", line?);
 	}
+
+	return Ok(());
 }
