48ae71cdeda5231df864498391aa35c2cd743ece
Jordan Dennis <jdenn105@gmail.com>
2022-12-23 09:50:27 +1000

    Learning to pattern match a little bit.

--- ipynb_to_py.rs
+++ ipynb_to_py.rs
@@ -1,23 +1,9 @@
 use std::fs::File;
-use std::io::{Write, BufReader, BufRead, Error};
+use std::io::{BufReader, Error, BufRead};
 
-
-// fn main() -> Result<(), Error> {
-    // let path = "lines.txt";
-// 
-    // let mut output = File::create(path)?;
-    // write!(output, "Rust is Fun")?;
-// 
-    // let input = File::open(path)?;
-    // let buffered = BufReader::new(input);
-// 
-    // for line in buffered.lines() {
-        // println!("{}", line?);
-    // }
-// 
-    // Ok(())
-// }
-
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
@@ -26,12 +12,18 @@ fn main() -> Result<(), Error> {
         panic!("Error: More than one argument recieved!");
     }
 
-    let file_name = &args[1];
-	let ipynb = File::open(file_name)?;
-	let buf_ipynb = BufReader::new(ipynb);
+    let file_name: &String = &args[1];
+	let ipynb: File = File::open(file_name)?;
+	let buf_ipynb: BufReader<File> = BufReader::new(ipynb);
 
 	for line in buf_ipynb.lines() {
-		println!("{}", line?);
+		match line {
+			Ok(line_str) => println!("{}", line_str),
+			Err(err) => println!("{}", err)
+		}
+		// if line.contains("soruce: [") {
+			// println!("{}", line?);
+		// }
 	}
 
 	return Ok(());
