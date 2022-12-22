0ddede54e8040ae0f2ff5643697c4e3c11300a0a
Jordan Dennis <jdenn105@gmail.com>
2022-12-23 08:44:58 +1000

    Adding a simple file to use for testing my program.

--- ipynb_to_py.rs
+++ ipynb_to_py.rs
@@ -2,7 +2,7 @@ fn main()
 {
     let file_name: Vec<String> = std::env::args().collect();
  
-    if file_name.len() > 1
+    if file_name.len() > 2
     {
         panic!("Error: More than one argument recieved!");
     }
