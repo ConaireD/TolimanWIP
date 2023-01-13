63c98268658e2f3a971943b99b5c12f8b324b99d
Jordan Dennis <jdenn105@gmail.com>
2023-01-13 13:43:19 +1000

    Finishing the implementation of `Background`.

--- src/toliman.py
+++ src/toliman.py
@@ -172,27 +172,27 @@ def _simulate_background_stars() -> None:
         phot_g_mean_flux IS NOT NULL
     """
 
-    bg_ra = 220.002540961 + 0.1
-    bg_dec = -60.8330381775
-    alpha_cen_flux = 1145.4129625806625
-    bg_win = 2. / 60. 
-    bg_rad = 2. / 60. * np.sqrt(2.)
+    bg_ra: float = 220.002540961 + 0.1
+    bg_dec: float = -60.8330381775
+    alpha_cen_flux: float = 1145.4129625806625
+    bg_win: float = 2. / 60. 
+    bg_rad: float = 2. / 60. * np.sqrt(2.)
 
-    bg_stars = Gaia.launch_job(conical_query.format(bg_ra, bg_dec, bg_rad))
+    bg_stars: object = Gaia.launch_job(conical_query.format(bg_ra, bg_dec, bg_rad))
 
-    bg_stars_ra = np.array(bg_stars.results["ra"])
-    bg_stars_dec = np.array(bg_stars.results["dec"])
-    bg_stars_flux = np.array(bg_stars.results["flux"])
+    bg_stars_ra: float = np.array(bg_stars.results["ra"]) - bg_ra
+    bg_stars_dec: float = np.array(bg_stars.results["dec"]) - bg_dec
+    bg_stars_flux: float = np.array(bg_stars.results["flux"])
 
-    ra_in_range = np.abs(bg_stars_ra - bg_ra) < bg_win
-    dec_in_range = np.abs(bg_stars_dec - bg_dec) < bg_win
-    in_range = ra_in_range & dec_in_range
-    sample_len = in_range.sum()
+    ra_in_range: float = np.abs(bg_stars_ra - bg_ra) < bg_win
+    dec_in_range: float = np.abs(bg_stars_dec - bg_dec) < bg_win
+    in_range: float = ra_in_range & dec_in_range
+    sample_len: float = in_range.sum()
 
-    bg_stars_ra_crop = bg_stars_ra[in_range]
-    bg_stars_dec_crop = bg_stars_dec[in_range]
-    bg_stars_flux_crop = bg_stars_flux[in_range]
-    bg_stars_rel_flux_crop = bg_stars_flux_crop / alpha_cen_flux
+    bg_stars_ra_crop: float = bg_stars_ra[in_range]
+    bg_stars_dec_crop: float = bg_stars_dec[in_range]
+    bg_stars_flux_crop: float = bg_stars_flux[in_range]
+    bg_stars_rel_flux_crop: float = bg_stars_flux_crop / alpha_cen_flux
 
     with open("datasheets/bg_stars.csv", "w") as sheet:
         sheet.write("ra,dec,rel_flux\n")
@@ -624,7 +624,7 @@ class AlphaCentauri(dl.BinarySource):
         """
         if not spectrum:
             with open(SPECTRUM_DIR, "r") as spectrum:
-                lines: list = open.readlines()
+                lines: list = open.readlines().remove(0)
 
                 strip: callable = lambda _str: _str.strip().split(",")
                 str_to_float: callable = lambda _str: float(_str.strip())
@@ -662,6 +662,29 @@ class Background(dl.MultiPointSource):
         """
         if not spectrum:
             spectrum: object = dl.ArraySpectrum(
-
+                wavelengths = np.linspace(
+                    FILTER_MIN_WAVELENGTH, 
+                    FILTER_MAX_WAVELENGTH, 
+                    FILTER_DEFAULT_RES
+                ),
+                fluxes = np.ones((FILTER_DEFAULT_RES,), dtype = float)
             )
+
+        with open(BACKGROUND_DIR, "r") as background:
+            lines: list = background.readlines().remove(0)
+            strip: callable = lambda _str: _str.strip().split(",")
+            str_to_float: callable = lambda _str: float(_str.strip())
+            entries: list = jax.tree_map(strip, lines)
+            _background: float = jax.tree_map(str_to_float, entries)
+
+        position: float = _background[:, (0, 1)]
+        flux: float = _background[:, 2]
+
+        super().__init__(
+            position = position,
+            flux = flux,
+            spectrum = spectrum
+        )
+
+            
         
