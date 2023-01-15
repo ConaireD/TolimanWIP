d40c06309e6cbd9c983dfd2cb4bae19fa2823c93
Jordan Dennis <jdenn105@gmail.com>
2023-01-16 08:49:54 +1000

    Starting to flesh out the tests.

--- tests/toliman_test.py
+++ tests/toliman_test.py
@@ -1,25 +1,77 @@
 import pytest
+import dLux as dl
+
+from toliman import (
+    TolimanOptics,
+    TolimanDetector,
+    AlphaCentauri,
+    Background
+)
 
 class TestTolimanOptics(object):
     def test___init__(self: object) -> None:
         # Operate in static mode
+        static_toliman: object = TolimanOptics(
+            operate_in_static_mode = True
+        )
+
+        optics: list = list(static_toliman.layers.values())
+        assert _contains_optic(optics, dl.StaticAperture)
 
         # Load a mask that is too large
         with pytest.expect(NotImplementedError):
+            toliman: object = TolimanOptics(
+                pixels_in_pupul = 2048
+            ) 
 
         # Mask sampling error
+        with pytest.expect(ValueError):
+            toliman: object = TolimanOptics(
+                pixels_in_pupil = 125
+            )
 
         # Incorrect file address error.
+        with pytest.expect(ValueError):
+            toliman: object = TolimanOptics(
+                path_to_mask = "i/don't/exist.npy"
+            )
 
         # Simulate aberrations (yes and no)
+        toliman: object = TolimanOptics(simulate_aberrations = True)
+        opitcs: list = list(toliman.layers.values())
+        assert _contains_instance(optics, dl.StaticAberratedAperture)
+
+        toliman: object = TolimanOptics(simulate_aberrations = False)
+        opitcs: list = list(toliman.layers.values())
+        assert not _contains_instance(optics, dl.StaticAberratedAperture)
 
         # Simulate polish
+        with pytest.expect(NotImplementedError):
+            toliman: object = TolimanOptics(
+                simulate_polish = True
+            )
 
         # Operate in Fresnel mode
         with pytest.expect(NotImplementedError):
+            toliman: object = TolimanOptics(operate_in_fresnel_mode = True)
 
 
     def test_insert(self: object) -> None:
+        # Test not an optical layer
+        toliman: object = TolimanOptics()
+        with pytest.expect(ValueError):
+            toliman.insert(0, 1)
+
+        optics: list = list(toliman.layers.values())
+        length: int = len(optics)
+
+        insertion: object = dl.HexagonalAperture(1.)
+        toliman: object = toliman.insert(0, insertion)
+
+        new_optics: list = list(toliman.layers.values())
+
+            
+
     def test_remove(self: object) -> None:
 
 
