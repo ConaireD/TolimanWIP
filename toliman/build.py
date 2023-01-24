48fa723e79239f520bb6486bc0f9dcdd4e1c2453
Jordan Dennis <jdenn105@gmail.com>
2023-01-25 08:24:57 +1000

    Documenting the `_accumulate_path` function (see #1 and #2).

--- toliman/build.py
+++ toliman/build.py
@@ -41,6 +41,30 @@ def _is_phoenix_installed() -> bool:
     return True
 
 def _accumulate_path(strings: list, paths: str = []) -> list:
+    """
+    Incrementally build a path from a list.
+
+    Parameters
+    ----------
+    strings: list 
+        A list of directories that can be pasted together to form a 
+        list.
+    paths: list
+        A list of the paths to each dir.
+
+    Returns
+    -------
+    paths: list
+        Starting with the root directory, which is assumed to be the 
+        first entry in `strings`, a list of directories growing from
+        root.
+
+    Examples
+    --------
+    >>> strings: list = ["root", "dev", "null"]
+    >>> _accumulate_path(strings)
+    ::: ["root", "root/dev", "root/dev/null"]
+    """
     if not strings:
         return paths
     else:
