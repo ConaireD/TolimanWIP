dda39735977c589fdea829c9791fe644a1b2f68c
Jordan Dennis <jdenn105@gmail.com>
2023-01-25 09:28:06 +1000

    Implementing the `_install_mask` function (see #1).

--- toliman/build.py
+++ toliman/build.py
@@ -7,6 +7,7 @@ __author__ = "Jordan Dennis"
 
 HOME: str = "toliman/assets/grid/phoenix"
 PHOENIX_HOME: str = "https://archive.stsci.edu/hlsps/reference-atlases/cdbs/grid/phoenix"
+MASK_HOME: str = "https://github.com/ConaireD/TolimanWIP/raw/mask/mask.npy"
 PHOENIXM00: str = "phoenixm00"
 PHOENIXP03: str = "phoenixp03"
 PHOENIX_PATHS: str = [
@@ -74,9 +75,35 @@ def _accumulate_path(strings: list, paths: str = []) -> list:
             paths.append(paths[-1] + "/" + strings.pop(0))
         return _accumulate_path(strings, paths)
 
-def _install_phoenix() -> bool:
+def _install_mask() -> bool:
     """
     Install phoenix from the web.
+    """
+    MASK: str = "toliman/assets"
+    if not os.path.exists(MASK):
+        for path in _accumulate_path(MASK.split("/")):
+            if not os.path.exists(path):
+                os.mkdir(path)
+
+    with open("{}/{}".format(MASK, "mask.npy", "wb") as file_dev:
+        response: iter = requests.get(MASK_HOME, stream=True)
+        total_size: int = int(response.headers.get('content-length', 0))
+
+        print("Downloading: {}.".format(MASK_HOME))
+
+        progress: object = tqdm.tqdm(
+            total=total_size,
+            unit='iB', 
+            unit_scale=True
+        )
+
+        for data in response.iter_content(1024):
+            progress.update(len(data))
+            file_dev.write(data)
+
+def _install_phoenix() -> bool:
+    """
+    Install the mask from the web.
     """
     if not os.path.exists(HOME):
         for path in _accumulate_path(HOME.split("/")):
