310a60013e8a34765d02e1c94eaa1faebc712f97
Jordan Dennis <jdenn105@gmail.com>
2023-01-24 15:07:10 +1000

    Writing the `_install_phoenix` function (see #1 and #2).

--- toliman/build.py
+++ toliman/build.py
@@ -5,15 +5,17 @@ __author__ = "Jordan Dennis"
 
 HOME: str = "toliman/assets/grid/phoenix"
 PHOENIX_HOME: str = "https://archive.stsci.edu/hlsps/reference-atlases/cdbs/grid/phoenix/"
+PHOENIXM00: str = "phoenixm00"
+PHOENIXP03: str = "phoenixp03"
 PHOENIX_PATHS: str = [
-    "phoenixm00/phoenixm00_5200.fits"
-    "phoenixm00/phoenixm00_5300.fits"
-    "phoenixm00/phoenixm00_5700.fits"
-    "phoenixm00/phoenixm00_5800.fits"
-    "phoenixp03/phoenixp03_5200.fits"
-    "phoenixp03/phoenixp03_5300.fits"
-    "phoenixp03/phoenixp03_5700.fits"
-    "phoenixp03/phoenixp03_5800.fits"
+    "{}/{}_5200.fits".format(PHOENIXM00),
+    "{}/{}_5300.fits".format(PHOENIXM00),
+    "{}/{}_5700.fits".format(PHOENIXM00),
+    "{}/{}_5800.fits".format(PHOENIXM00),
+    "{}/{}_5200.fits".format(PHOENIXP03),
+    "{}/{}_5300.fits".format(PHOENIXP03),
+    "{}/{}_5700.fits".format(PHOENIXP03),
+    "{}/{}_5800.fits".format(PHOENIXP03),
     "catalog.fits"
 ]
 
@@ -27,7 +29,6 @@ def _is_phoenix_installed() -> bool:
 
     return True
 
-
 def _accumulate_path(strings: list, paths: str = []) -> list:
     if not strings:
         return paths
@@ -40,19 +41,21 @@ def _accumulate_path(strings: list, paths: str = []) -> list:
 
 def _install_phoenix() -> bool:
     if not os.path.exists(HOME):
-        for 
-        if not os.path.exists("toliman"):
-            os.mkdir("toliman")
-        
-        if not os.path.exists("toliman/assets"):
-            os.mkdir("toliman/assets")
+        for path in _accumulate_path(HOME.split("/")):
+            if not os.path.exists(path):
+                os.mkdir(path)
 
-        if not os.path.exists("toliman/assets/grid"):
-            os.mkdir("toliman/assets/grid")
+    for path in [PHOENIXM00, PHOENIXP03]:
+        rel_path: str = "{}/{}".format(HOME, path)
+        if not os.path.exists(rel_path):
+            os.mkdir(rel_path)
 
-        if not os.path.exists("toliman/assets/grid/phoenix"):
-            os.mkdir("toliman/assets/grid/phoenix")
-
-    if not os.is_file()
+    for file in PHOENIX_PATHS:
+        path: str = "{}/{}".format(HOME, file)
+    
+        if not os.is_file(path):
+            with open(path, "w") as file:
+                url: str = "{}/{}".format(PHOENIX_HOME, file)
+                file.write(requests.request("GET", url))
 
 
