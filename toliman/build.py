9edbcf04eb3b98b801e54521c913b89b0b3b432b
Jordan Dennis <jdenn105@gmail.com>
2023-01-25 08:40:13 +1000

    Moving the `_simulate_...` functions into `build.py` (see #1).

--- toliman/build.py
+++ toliman/build.py
@@ -111,6 +111,131 @@ def _install_phoenix() -> bool:
                     progress.update(len(data))
                     file_dev.write(data)
 
+def _simulate_alpha_cen_spectra(number_of_wavelengths: int = 25) -> None:
+    """
+    Simulate the spectrum of the alpha centauri binary using `pysynphot`.
+
+    The output is saved to a file so that
+    it can be used again later without having to be reloaded.
+
+    Parameters
+    ----------
+    number_of_wavelengts: int
+        The number of wavelengths that you wish to use for the simulation.
+        The are taken from the `pysynphot` output by binning.
+    """
+    import pysynphot
+
+    os.environ["PYSYN_CDBS"] = "/home/jordan/Documents/toliman/toliman/assets"
+
+    def angstrom_to_m(angstrom: float) -> float:
+        m_per_angstrom: float = 1e-10
+        return m_per_angstrom * angstrom
+
+    alpha_cen_a_spectrum: float = pysynphot.Icat(
+        "phoenix",
+        ALPHA_CEN_A_SURFACE_TEMP,
+        ALPHA_CEN_A_METALICITY,
+        ALPHA_CEN_A_SURFACE_GRAV,
+    )
+
+    alpha_cen_b_spectrum: float = pysynphot.Icat(
+        "phoenix",
+        ALPHA_CEN_B_SURFACE_TEMP,
+        ALPHA_CEN_B_METALICITY,
+        ALPHA_CEN_B_SURFACE_GRAV,
+    )
+
+    WAVES: int = 0
+    ALPHA_CEN_A: int = 1
+    ALPHA_CEN_B: int = 2
+
+    spectra: float = np.array([
+            angstrom_to_m(alpha_cen_a_spectrum.wave),
+            _normalise(alpha_cen_a_spectrum.flux),
+            _normalise(alpha_cen_b_spectrum.flux),
+        ], dtype=float)
+
+    del alpha_cen_a_spectrum, alpha_cen_b_spectrum
+
+    decision: bool = np.logical_and(
+        (FILTER_MIN_WAVELENGTH < spectra[WAVES]),
+        (spectra[WAVES] < FILTER_MAX_WAVELENGTH)
+    )
+
+    spectra: float = spectra[:, decision]
+
+    del decision
+
+    size: int = spectra[WAVES].size
+    resample_size: int = size - size % number_of_wavelengths
+    spectra: float = spectra[:, :resample_size]
+    resample_by: int = resample_size // number_of_wavelengths 
+    spectra: float = _downsample_along_axis(spectra, resample_by, axis=1)
+
+    with open("toliman/assets/spectra.csv", "w") as fspectra:
+        fspectra.write("alpha cen a waves (m), ")
+        fspectra.write("alpha cen a flux (W/m/m), ")
+        fspectra.write("alpha cen b flux (W/m/m)\n")
+
+        for i in np.arange(number_of_wavelengths, dtype=int):
+            fspectra.write("{}, ".format(spectra[WAVES][i]))
+            fspectra.write("{}, ".format(spectra[ALPHA_CEN_A][i]))
+            fspectra.write("{}\n".format(spectra[ALPHA_CEN_B][i]))
+
+def _simulate_background_stars() -> None:
+    """
+    Sample the Gaia database for typical background stars.
+
+    The primary use of this function is to
+    build a sample that can be used to look for biases.
+    """
+    from astroquery.gaia import Gaia
+
+    conical_query = """
+    SELECT
+        TOP 12000 
+        ra, dec, phot_g_mean_flux AS flux
+    FROM
+        gaiadr3.gaia_source
+    WHERE
+        CONTAINS(POINT('', ra, dec), CIRCLE('', {}, {}, {})) = 1 AND
+        phot_g_mean_flux IS NOT NULL
+    """
+
+    bg_ra: float = 220.002540961 + 0.1
+    bg_dec: float = -60.8330381775
+    alpha_cen_flux: float = 1145.4129625806625
+    bg_win: float = 2.0 / 60.0
+    bg_rad: float = 2.0 / 60.0 * np.sqrt(2.0)
+
+    bg_stars: object = Gaia.launch_job(conical_query.format(bg_ra, bg_dec, bg_rad))
+
+    bg_stars_ra: float = np.array(bg_stars.results["ra"]) - bg_ra
+    bg_stars_dec: float = np.array(bg_stars.results["dec"]) - bg_dec
+    bg_stars_flux: float = np.array(bg_stars.results["flux"])
+
+    ra_in_range: float = np.abs(bg_stars_ra) < bg_win
+    dec_in_range: float = np.abs(bg_stars_dec) < bg_win
+    in_range: float = ra_in_range & dec_in_range
+    sample_len: float = in_range.sum()
+
+    bg_stars_ra_crop: float = bg_stars_ra[in_range]
+    bg_stars_dec_crop: float = bg_stars_dec[in_range]
+    bg_stars_flux_crop: float = bg_stars_flux[in_range]
+    bg_stars_rel_flux_crop: float = bg_stars_flux_crop / alpha_cen_flux
+
+    print(sample_len)
+
+    with open("toliman/assets/background.csv", "w") as sheet:
+        sheet.write("ra,dec,rel_flux\n")
+        for row in np.arange(sample_len):
+            sheet.write(f"{bg_stars_ra_crop[row]},")
+            sheet.write(f"{bg_stars_dec_crop[row]},")
+            sheet.write(f"{bg_stars_rel_flux_crop[row]}\n")
+
+
+
 def main():
     print("Building `toliman`!")
     print("-------------------")
