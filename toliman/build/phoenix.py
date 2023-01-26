7358aa563bfe0725e60dd2c5f5944a4991f582a0
Jordan Dennis <jdenn105@gmail.com>
2023-01-27 09:16:52 +1000

    This is the most complex of the files in the build module.

---
+++ toliman/build/phoenix.py
@@ -0,0 +1,156 @@
+import requests
+import sys 
+import os
+import paths
+
+__author__ = "Jordan Dennis"
+__all__ = ["_is_phoenix_installed", "_install_phoenix"]
+
+HOME: str = "grid/phoenix"
+URL: str = "https://archive.stsci.edu/hlsps/reference-atlases/cdbs/grid/phoenix"
+M00: str = "phoenixm00"
+P03: str = "phoenixp03"
+PATHS: str = [
+    "{}/{}_5200.fits".format(PHOENIXM00, PHOENIXM00),
+    "{}/{}_5300.fits".format(PHOENIXM00, PHOENIXM00),
+    "{}/{}_5700.fits".format(PHOENIXM00, PHOENIXM00),
+    "{}/{}_5800.fits".format(PHOENIXM00, PHOENIXM00),
+    "{}/{}_5200.fits".format(PHOENIXP03, PHOENIXP03),
+    "{}/{}_5300.fits".format(PHOENIXP03, PHOENIXP03),
+    "{}/{}_5700.fits".format(PHOENIXP03, PHOENIXP03),
+    "{}/{}_5800.fits".format(PHOENIXP03, PHOENIXP03),
+    "catalog.fits"
+]
+
+
+def is_phoenix_installed(root: str) -> bool:
+    """
+    Check if "phoenix" is installed.
+
+    Returns
+    -------
+    installed: bool
+        True if all the phoenix files are present else false.
+    """
+    home_path: str = "{}/{}".format(root, HOME)
+    if not os.path.exists(home_path):
+        return False
+
+    for path in PHOENIX_PATHS:
+        rel_path: str = "{}/{}".format(home_path, path)
+        if not os.path.isfile(rel_path):
+            print(rel_path)
+            return False
+
+    return True
+
+def install_phoenix(root: str) -> bool:
+    """
+    Install the mask from the web.
+    """
+    home_path: str = "{}/{}".format(root, HOME)
+    if not os.path.exists(home_path):
+        for path in _accumulate_path(home_path.split("/")):
+            if not os.path.exists(path):
+                os.mkdir(path)
+
+    for path in [PHOENIXM00, PHOENIXP03]:
+        rel_path: str = "{}/{}".format(home_path, path)
+        if not os.path.exists(rel_path):
+            os.mkdir(rel_path)
+
+    for file in PHOENIX_PATHS:
+        path: str = "{}/{}".format(home_path, file)
+    
+        if not os.path.isfile(path):
+            url: str = "{}/{}".format(PHOENIX_HOME, file)
+
+            with open(path, "wb") as file_dev:
+                url: str = "{}/{}".format(PHOENIX_HOME, file)
+                response: iter = requests.get(url, stream=True)
+                total_size: int = int(response.headers.get('content-length', 0))
+
+                print("Downloading: {}.".format(url))
+
+                progress: object = tqdm.tqdm(
+                    total=total_size,
+                    unit='iB', 
+                    unit_scale=True
+                )
+
+                for data in response.iter_content(1024):
+                    progress.update(len(data))
+                    file_dev.write(data)
+
+def simulate_alpha_cen_spectra(number_of_wavelengths: int = 25) -> None:
+    """
+    Simulate the spectrum of the alpha centauri binary using `pysynphot`.
+
+    The output is saved to a file so that
+    it can be used again later without having to be reloaded.
+
+    Parameters
+    ----------
+    number_of_wavelengts: int
+        The number of wavelengths that you wish to use for the simulation.
+        The are taken from the `pysynphot` output by binning.
+    """
+    import pysynphot
+
+    os.environ["PYSYN_CDBS"] = "/home/jordan/Documents/toliman/toliman/assets"
+
+    def angstrom_to_m(angstrom: float) -> float:
+        m_per_angstrom: float = 1e-10
+        return m_per_angstrom * angstrom
+
+    alpha_cen_a_spectrum: float = pysynphot.Icat(
+        "phoenix",
+        ALPHA_CEN_A_SURFACE_TEMP,
+        ALPHA_CEN_A_METALICITY,
+        ALPHA_CEN_A_SURFACE_GRAV,
+    )
+
+    alpha_cen_b_spectrum: float = pysynphot.Icat(
+        "phoenix",
+        ALPHA_CEN_B_SURFACE_TEMP,
+        ALPHA_CEN_B_METALICITY,
+        ALPHA_CEN_B_SURFACE_GRAV,
+    )
+
+    WAVES: int = 0
+    ALPHA_CEN_A: int = 1
+    ALPHA_CEN_B: int = 2
+
+    spectra: float = np.array([
+            angstrom_to_m(alpha_cen_a_spectrum.wave),
+            _normalise(alpha_cen_a_spectrum.flux),
+            _normalise(alpha_cen_b_spectrum.flux),
+        ], dtype=float)
+
+    del alpha_cen_a_spectrum, alpha_cen_b_spectrum
+
+    decision: bool = np.logical_and(
+        (FILTER_MIN_WAVELENGTH < spectra[WAVES]),
+        (spectra[WAVES] < FILTER_MAX_WAVELENGTH)
+    )
+
+    spectra: float = spectra[:, decision]
+
+    del decision
+
+    size: int = spectra[WAVES].size
+    resample_size: int = size - size % number_of_wavelengths
+    spectra: float = spectra[:, :resample_size]
+    resample_by: int = resample_size // number_of_wavelengths 
+    spectra: float = _downsample_along_axis(spectra, resample_by, axis=1)
+
+    with open("toliman/assets/spectra.csv", "w") as fspectra:
+        fspectra.write("alpha cen a waves (m), ")
+        fspectra.write("alpha cen a flux (W/m/m), ")
+        fspectra.write("alpha cen b flux (W/m/m)\n")
+
+        for i in np.arange(number_of_wavelengths, dtype=int):
+            fspectra.write("{}, ".format(spectra[WAVES][i]))
+            fspectra.write("{}, ".format(spectra[ALPHA_CEN_A][i]))
+            fspectra.write("{}\n".format(spectra[ALPHA_CEN_B][i]))
+
