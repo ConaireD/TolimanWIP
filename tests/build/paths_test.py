960ca180b8981ecc32c011a901a2461efe65a605
Jordan Dennis <jdenn105@gmail.com>
2023-01-27 15:57:30 +1000

    Partially writing tests for the `toliman.build.paths` module (see #1).

--- tests/build/paths_test.py
+++ tests/build/paths_test.py
@@ -1,3 +1,12 @@
+from toliman.build.paths import (
+    accumulate,
+    concat
+)
+
+from pytest.mark import (
+    parametrize
+)
+
 def test_accumulate_on_empty_entry():
     # Arrange
     entry: list = []
@@ -9,8 +18,35 @@ def test_accumulate_on_empty_entry():
     assert out == []
 
 def test_accumulate_on_single_entry():
+    # Arrange 
+    entry: list = ["hi"]
+
+    # Act 
+    out: list = accumulate(entry)
+
+    # Assert
+    assert out == ["hi"]
+
 def test_accumulate_on_multiple_entries(): # TODO: parametrise this.
+    # Arrange
+    entry: list = ["hi", "there", "how"]
+
+    # Act
+    out: list = accumulate(entry)
+
+    # Assert 
+    assert out == ["hi", "hi/there", "hi/there/how"]
 
 def test_concat_on_empty_entry():
+    # Arrange
+    entry: list = []
+
+    # Act
+    out: list = concat(entry)
+
+    # Assert
+    assert out == ""
+
 def test_concat_on_single_entry():
+
 def test_concet_on_multiple_entries(): # TODO: parametrise this
