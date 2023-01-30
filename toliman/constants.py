c45e65ce8f4d979706bf0645d24c21a4a848367e
Jordan Dennis <jdenn105@gmail.com>
2023-01-31 08:40:17 +1000

    Starting to build a better interface for the constants on top of `os.environ` (see #1).

--- toliman/constants.py
+++ toliman/constants.py
@@ -1,40 +1,47 @@
 import os
 import dLux as dl
+import jax.numpy as np
 
-os.environ["DEFAULT_PUPIL_NPIX"]: int = "256"
-os.environ["DEFAULT_DETECTOR_NPIX"]: int = "128"
-os.environ["DEFAULT_NUMBER_OF_ZERNIKES"]: int = "5"
+def set_const(const: str, value: object) -> None:
+    os.environ[const] = str(value)
 
-os.environ["DEFAULT_MASK_DIR"]: str = "/home/jordan/Documents/toliman/toliman/assets/mask.npy"
-os.environ["SPECTRUM_DIR"]: str = "/home/jordan/Documents/toliman/toliman/assets/spectra.csv"
-os.environ["BACKGROUND_DIR"]: str = "toliman/assets/background.csv"
+def is_const_defined(const: str) -> bool:
+    if os.environ[const]:
+        return True
+    return False
 
-os.environ["TOLIMAN_PRIMARY_APERTURE_DIAMETER"]: float = "0.13"
-os.environ["TOLIMAN_SECONDARY_MIRROR_DIAMETER"]: float = "0.032"
-os.environ["TOLIMAN_DETECTOR_PIXEL_SIZE"]: float = "dl.utils.arcseconds_to_radians(0.375)"
-os.environ["TOLIMAN_WIDTH_OF_STRUTS"]: float = "0.01"
-os.environ["TOLIMAN_NUMBER_OF_STRUTS"]: int = "3"
+def get_const_as_type(const: str, t: type) -> str:
+    return t(os.environ[const])
 
-os.environ["DEFAULT_DETECTOR_JITTER"]: float = "2.0"
-os.environ["DEFAULT_DETECTOR_SATURATION"]: float = "2500"
-os.environ["DEFAULT_DETECTOR_THRESHOLD"]: float = "0.05"
+if not os.environ["TOLIMAN_HOME"]:
+    warnings.warn("`TOLIMAN_HOME` is not defined. Using `.assets`.")
+    set_const("TOLIMAN_HOME", ".assets")
 
-os.environ["ALPHA_CENTAURI_SEPARATION"]: float = "dl.utils.arcseconds_to_radians(8.0)"
-os.environ["ALPHA_CENTAURI_POSITION"]: float = "np.array([0.0, 0.0], dtype=float)"
-os.environ["ALPHA_CENTAURI_MEAN_FLUX"]: float = "1.0"
-os.environ["ALPHA_CENTAURI_CONTRAST"]: float = "2.0"
-os.environ["ALPHA_CENTAURI_POSITION_ANGLE"]: float = "0.0"
-
-os.environ["ALPHA_CEN_A_SURFACE_TEMP"]: float = "5790.0"
-os.environ["ALPHA_CEN_A_METALICITY"]: float = "0.2"
-os.environ["ALPHA_CEN_A_SURFACE_GRAV"]: float = "4.0"
-
-os.environ["ALPHA_CEN_B_SURFACE_TEMP"]: float = 5260.0
-os.environ["ALPHA_CEN_B_METALICITY"]: float = 0.23
-os.environ["ALPHA_CEN_B_SURFACE_GRAV"]: float = 4.37
-
-os.environ["FILTER_MIN_WAVELENGTH"]: float = 595e-09
-os.environ["FILTER_MAX_WAVELENGTH"]: float = 695e-09
-os.environ["FILTER_DEFAULT_RES"]: int = 24
-
-
+set_const("DEFAULT_PUPIL_NPIX", 256)
+set_const("DEFAULT_DETECTOR_NPIX", 128)
+set_const("DEFAULT_NUMBER_OF_ZERNIKES", 5)
+set_const("DEFAULT_MASK_DIR", "{}/mask.npy".format(os.environ["TOLIMAN_HOME"]))
+set_const("SPECTRUM_DIR", "{}/spectra.csv".format(os.environ["TOLIMAN_HOME"]))
+set_const("BACKGROUND_DIR", "{}/background.csv".format(os.environ["TOLIMAN_HOME"]))
+set_const("TOLIMAN_PRIMARY_APERTURE_DIAMETER", "0.13")
+set_const("TOLIMAN_SECONDARY_MIRROR_DIAMETER", "0.032")
+set_const("TOLIMAN_DETECTOR_PIXEL_SIZE", "dl.utils.arcseconds_to_radians(0.375)")
+set_const("TOLIMAN_WIDTH_OF_STRUTS", "0.01")
+set_const("TOLIMAN_NUMBER_OF_STRUTS", "3")
+set_const("DEFAULT_DETECTOR_JITTER", "2.0")
+set_const("DEFAULT_DETECTOR_SATURATION", "2500")
+set_const("DEFAULT_DETECTOR_THRESHOLD", "0.05")
+set_const("ALPHA_CENTAURI_SEPARATION", "dl.utils.arcseconds_to_radians(8.0)")
+set_const("ALPHA_CENTAURI_POSITION", "np.array([0.0, 0.0], dtype=float)")
+set_const("ALPHA_CENTAURI_MEAN_FLUX", "1.0")
+set_const("ALPHA_CENTAURI_CONTRAST", "2.0")
+set_const("ALPHA_CENTAURI_POSITION_ANGLE", "0.0")
+set_const("ALPHA_CEN_A_SURFACE_TEMP", "5790.0")
+set_const("ALPHA_CEN_A_METALICITY", "0.2")
+set_const("ALPHA_CEN_A_SURFACE_GRAV", "4.0")
+set_const("ALPHA_CEN_B_SURFACE_TEMP", "5260.0")
+set_const("ALPHA_CEN_B_METALICITY", "0.23")
+set_const("ALPHA_CEN_B_SURFACE_GRAV", "4.37")
+set_const("FILTER_MIN_WAVELENGTH", "595e-09")
+set_const("FILTER_MAX_WAVELENGTH", "695e-09")
+set_const("FILTER_DEFAULT_RES", "24")
