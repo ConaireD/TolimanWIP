04e31b9d18740e11c3f28e24cef8b73e722958a6
Jordan Dennis <jdenn105@gmail.com>
2023-01-30 10:45:23 +1000

    Adding tests for `download_file_from_https`.

--- tests/build/https_test.py
+++ tests/build/https_test.py
@@ -30,8 +30,6 @@ def test_download_byte_from_https_takes_byte():
     # Assert
     if not os.path.isfile(path):
         raise ValueError
-
-    print(os.stat(path).st_size)
     
     assert 0 < os.stat(path).st_size < 2000
 
@@ -61,6 +59,49 @@ def test_download_byte_from_https_skips_existing():
     # Clean Up
     shutil.rmtree("tmp")
 
-#def test_download_file_from_https_takes_file():
-#def test_download_file_from_https_skips_existing():
-#def test_download_file_from_https_on_failure():
+def test_download_file_from_https_takes_file():
+    # Arrange
+    if os.path.exists("tmp"):
+        shutil.rmtree("tmp")
+
+    os.mkdir("tmp")
+    path: str = "tmp/jordan-dennis.html"
+    url: str = "https://jordan-dennis.github.io"
+
+    # Act
+    download_file_from_https(path, url)
+
+    # Assert
+    if not os.path.isfile(path):
+        raise ValueError
+
+    with open(path, "r") as file:
+        source: str = file.read()
+        assert source.startswith("<!DOCTYPE html>")
+        assert source.endswith("</body>")
+
+    # Clean Up
+    shutil.rmtree("tmp")
+
+def test_download_file_from_https_skips_existing():
+    # Arrange
+    if os.path.exists("tmp"):
+        shutil.rmtree("tmp")
+
+    os.mkdir("tmp")
+    path: str = "tmp/jordan-dennis.html"
+    url: str = "https://jordan-dennis.github.io"
+    entry: str = "Hello world!"
+
+    with open(path, "w") as file:
+        file.write(entry)
+
+    # Act
+    download_file_from_https(path, url)
+
+    # Assert
+    with open(path, "r") as file:
+        assert entry == file.read()
+
+    # Clean Up
+    shutil.rmtree("tmp")
