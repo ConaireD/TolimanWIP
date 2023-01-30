083b40703f1318eff654f71ddd3e30a39b7e5951
Jordan Dennis <jdenn105@gmail.com>
2023-01-30 11:35:32 +1000

    Implementing tests for `is_phoenix_installed` (see #1).

--- tests/build/phoenix_test.py
+++ tests/build/phoenix_test.py
@@ -1,5 +1,8 @@
+import pytest
+import toliman.build.phoenix as phoenix
+
 # TODO: Make into fixtures
-ASSETS: str = "tests/assets"
+ASSETS: str = "tests/.assets"
 PHOENIX: str = "{}/grid/phoenix".format(ASSETS)
 PHOENIXS: str = ["phoenixm00", "phoenixp03"]
 NUMBERS: list = [5200, 5300, 5700, 5800]
@@ -33,13 +36,42 @@ def make_phoenix_type_files(phoenix: str) -> None:
                 continue
 
 def make_phoenix_catalog() -> None:
+    if not os.path.exsists(PHOENIX)
+        make_phoenix_root_directory()
     path: str = "{}/catalog.fits".format(PHOENIX)
     if not os.path.exists(path):
         with open(path, "w") as file:
             pass
 
+def make_phoenix_installed() -> None:
+    os.mkdir(ASSETS)
+    make_phoenix_catalog()
+    for phoenix in PHOENIXS:
+        make_phoenix_type_files(phoenix)
+
 def remove_phoenix() -> None:
     if os.path.exists(ASSETS):
         shutil.rmtree(ASSETS)
 
-def 
+def test_is_phoenix_installed_when_fully_installed():
+    # Arrange 
+    make_phoenix_installed()
+
+    # Assert
+    assert phoenix.is_phoenix_installed()
+    remove_phoenix()
+
+def test_is_phoenix_installed_when_fully_installed():
+    # Arrange 
+    os.mkdir(ASSETS)
+    make_phoenix_catalog()
+
+    # Assert
+    assert not phoenix.is_phoenix_installed()
+    
+def test_is_phoenix_installed_when_not_installed():
+    # Arrange
+    remove_phoenix()
+
+    # Assert
+    assert not phoenix.is_phoenix_installed()
