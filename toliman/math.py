f1e069e047e7e422cdf76ed4961c4c0ca55b4507
Jordan Dennis <jdenn105@gmail.com>
2023-01-31 10:55:35 +1000

    Making a mistake copying file contents.

--- toliman/math.py
+++ toliman/math.py
@@ -1,152 +1,32 @@
-import jax.numpy as np
-
-__author__ = [
-    "Jordan Dennis",
-    "Louis Desdoigts",
-]
-
-__all__ = [
-    "downsample_square_grid", 
-    "downsample_along_axis", 
-    "simulate_data",
-    "pixel_response",
-    "photon_noise",
-    "latent_detector_noise",
-]
-
-def downsample_square_grid(arr: float, m: int) -> float:
+def normalise(arr: float) -> float:
     """
-    Resample a square array by a factor of `m`.
+    Rescale and array onto [0, 1].
 
     Parameters
     ----------
     arr: float
-        An `NxN` array.
-    m: float
-        The factor to downsample by so that the final shape is `(N/m)x(N/m)`.
-        This implies that `N % m == 0`.
-
-    Examples
-    --------
-    ```python
-    >>> import jax.numpy as np
-    >>> up_arr: float = np.ones((1024, 1024), dtype=float)
-    >>> down_arr: float = _downsample(arr, 4)
-    >>> down_arr.shape
-    (256, 256)
-    ```
-    """
-    n: int = arr.shape[0]
-    out: int = n // m
-
-    dim_one: float = arr.reshape((n * out, m)).sum(1).reshape(n, out).T
-    dim_two: float = dim_one.reshape((out * out, m)).sum(1).reshape(out, out).T
-
-    return dim_two / m / m
-
-def downsample_along_axis(arr: float, m: int, axis: int = 0) -> float:
-    """
-    Resampling an array by averaging along a particular dimension.
-
-    Parameters
-    ----------
-    arr: float
-        The array to resample.
-    m: int
-        The factor by which to downsample the axis.
-    axis: int = 0
-        The axis to resample.
+        Any array.
 
     Returns
     -------
     arr: float
-        The resampled array
+        An array of floating point numbers over the range [0, 1].
     """
-    shape: tuple = arr.shape
-    n: int = shape[axis]
-    out: int = n // m
-    new: tuple = tuple([out if i == axis else dim for i, dim in enumerate(shape)] + [m])
-    return arr.reshape(new).sum(-1) / m
-
-def simulate_data(model: object, scale: float) -> float:
+    return (arr - arr.min()) / arr.ptp()
+        
+def angstrom_to_m(angstrom: float) -> float:
     """
-    Simulate some fake sata for comparison.
+    Convert an array that is in angstrom to meters.
 
     Parameters
     ----------
-    model: object
-        A model of the toliman. Should inherit from `dl.Instrument` or
-        be an instance.
-    scale: float
-        How noisy is the detector?
+    angstrom: float, angstrom
+        An array of measurements.
 
     Returns
     -------
-    data: float, photons
-        A noisy psf.
+    meters: float, meters
+        An array of measurements.
     """
-    psf: float = model.model()
-    noisy_psf: float = photon_noise(psf)
-    noisy_image: float = noisy_psf + latent_detector_noise(scale, psf.shape)
-    return noisy_image
-
-def pixel_response(shape: float, threshold: float, seed: int = 1) -> float:
-    """
-    Simulate pixel reponses.
-
-    Parameters
-    ----------
-    shape: tuple[int]
-        The array shape to populate with a random pixel response.
-    threshold: float
-        How far from 1. does the pixel response typically vary.
-    seed: int = 1
-        The seed of the random generation.
-
-    Returns
-    -------
-    pixel_response: float
-        An array of the pixel responses.
-    """
-    key: object = jax.random.PRNGKey(seed)
-    return 1.0 + threshold * jax.random.normal(key, shape)
-
-def photon_noise(psf: float, seed: int = 0) -> float:
-    """
-    Simulate photon noise.
-
-    Parameters
-    ----------
-    psf: float
-        The psf on which to add photon noise.
-    seed: int = 1
-        The seed of the random generation.
-
-    Returns
-    -------
-    photon_noise: float
-        A noisy psf.
-    """
-    key = jax.random.PRNGKey(seed)
-    return jax.random.poisson(key, psf)
-
-def latent_detector_noise(scale: float, shape: float, seed: int = 0) -> float:
-    """
-    Simulate some gaussian latent noise.
-
-    Parameters
-    ----------
-    scale: float, photons
-        The standard deviation of the gaussian in photons.
-    shape: tuple
-        The shape of the array to generate the noise on.
-    seed: int = 0
-        The seed of the random generation.
-
-    Returns
-    -------
-    det_noise: float, photons
-        The an additional noise source from the detector.
-    """
-    key: object = jax.random.PRNGKey(seed)
-    return scale * jax.random.normal(key, shape)
+    m_per_angstrom: float = 1e-10
+    return m_per_angstrom * angstrom
