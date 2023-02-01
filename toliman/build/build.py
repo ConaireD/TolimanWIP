3f5cfb7631571a6fa396777fc6cb76f78783a3ea
Jordan Dennis <jdenn105@gmail.com>
2023-02-01 11:10:34 +1000

    Implementing `is_toliman_installed` (see #1).

--- toliman/build/build.py
+++ toliman/build/build.py
@@ -1,12 +1,68 @@
 import os
-import sys
 import warnings
-import tqdm
-import requests
+import toliman.build.mask as mask
+import toliman.build.phoenix as phoenix
+import toliman.build.background as bg
+import toliman.constants as const
 
 __author__ = "Jordan Dennis"
+__all__ = [
+    "is_toliman_installed",
+    "install_toliman", # TODO: force
+]
 
-def build():
+TOLIMAN_HOME: str = const.get_const_as_type("TOLIMAN_HOME", str) 
+
+def is_toliman_installed(root: str = TOLIMAN_HOME) -> bool:
+    """
+    Checks if all the assets files for toliman are installed.
+
+    Parameters
+    ----------
+    root: str = TOLIMAN_HOME
+        The directory to search in. This should be TOLIMAN_HOME 
+        but I have enabled other options if multiple installations 
+        exist.
+
+    Returns
+    -------
+    installed: bool
+        True if all the components are installed, else False.
+
+    Examples
+    --------
+    >>> import toliman.build as build
+    >>> import toliman.build.mask as mask
+    >>> import toliman.build.phoenix as phoenix
+    >>> import toliman.build.background as bg
+    >>> import shutil 
+    >>> import os
+    >>> if os.path.isdir("tmp"):
+    ...     shutil.rmtree("tmp")
+    >>> os.mkdir("tmp")
+    >>> mask.install_mask("tmp")
+    >>> mask.is_mask_installed("tmp")
+    ::: True
+    >>> phoenix.is_phoenix_installed("tmp")
+    ::: False
+    >>> bg.is_background_installed("tmp")
+    ::: False
+    >>> build.is_toliman_installed("tmp")
+    ::: False
+    >>> phoenix.install_phoenix("tmp")
+    >>> bg.install_background_stars("tmp")
+    >>> build.is_toliman_installed("tmp")
+    ::: True
+    """
+    component_installations: list = [
+        phoenix.is_phoenix_installed(),
+        mask.is_mask_installed(),
+        bg.are_background_stars_installed(),
+    ]
+
+    return all(component_installations)
+
+def build(root: str = TOLIMAN_HOME):
     print("Building `toliman`!")
     print("-------------------")
 
