509857f98ccb20ef2005fdb16ef281cff2fbb2e9
Jordan Dennis <jdenn105@gmail.com>
2023-02-06 11:12:18 +1000

    Refactoring and documenting `test_install_mask_creates_file` (see #1 and #2).

--- tests/build/test_mask.py
+++ tests/build/test_mask.py
@@ -53,10 +53,27 @@ def test_is_mask_installed_when_not_installed(
     """
     assert not mask.is_mask_installed(root)
 
-def test_install_mask_creates_file() -> None:
+@pytest.mark.parametrize("root", [ROOT])
+def test_install_mask_creates_file(
+        root: str,
+        remove_installation: fixture[None],
+    ) -> None:
+    """
+    Does mask.install_mask create a file?
 
-    mask.install_mask(ROOT)
-    assert os.path.isfile(paths.concat([ROOT, "mask.npy"]))
+    Fixtures
+    --------
+    remove_installation: fixture[None],
+        Ensures there is no installation before and after the test.
+
+    Parameters
+    ----------
+    root: str = ROOT
+        Where to look for the installation. Directly parametrises 
+        mask.is_mask_installed. Indirectly parametrizes remove_installation.
+    """
+    mask.install_mask(root)
+    assert os.path.isfile(paths.concat([root, "mask.npy"]))
 
 def test_install_mask_has_correct_shape() -> None:
     mask.install_mask(ROOT)
