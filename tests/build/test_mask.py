6bd6af2e5299bb04a373c713c812d565ddf73953
Jordan Dennis <jdenn105@gmail.com>
2023-02-06 11:05:59 +1000

    Refactoring and documenting `test_is_mask_installed_when_not_installed` (see #1 and #2).

--- tests/build/test_mask.py
+++ tests/build/test_mask.py
@@ -32,34 +32,31 @@ def test_is_mask_installed_when_installed(
     open(paths.concat([root, "mask.npy"]), "w").close()
     assert mask.is_mask_installed(root)
 
-def test_is_mask_installed_when_not_installed() -> None:
-    # Arrange
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
+@pytest.mark.parametrize("root", [ROOT])
+def test_is_mask_installed_when_not_installed(
+        root: str,
+        remove_installation: fixture[None],
+    ) -> None:
+    """
+    Can mask.is_mask_installed identify no installation?
 
-    os.mkdir(ROOT)
+    Fixtures
+    --------
+    remove_installation: fixture[None],
+        Ensures there is no installation before and after the test.
 
-    # Act 
-    assert not mask.is_mask_installed(ROOT)
-
-    # Clean Up
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
+    Parameters
+    ----------
+    root: str = ROOT
+        Where to look for the installation. Directly parametrises 
+        mask.is_mask_installed. Indirectly parametrizes remove_installation.
+    """
+    assert not mask.is_mask_installed(root)
 
 def test_install_mask_creates_file() -> None:
-    # Arrange
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
 
-    # Act
     mask.install_mask(ROOT)
-
-    # Assert
     assert os.path.isfile(paths.concat([ROOT, "mask.npy"]))
-
-    # Clean Up
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
 
 def test_install_mask_has_correct_shape() -> None:
     mask.install_mask(ROOT)
