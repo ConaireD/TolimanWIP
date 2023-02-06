d17b232a0bd2fc0628f90720bdab7496f903831e
Jordan Dennis <jdenn105@gmail.com>
2023-02-06 10:57:48 +1000

    Refactoring and documenting `test_is_mask_installed_when_installed` (see #1 and #2).

--- tests/build/test_mask.py
+++ tests/build/test_mask.py
@@ -4,23 +4,33 @@ import toliman.build.mask as mask
 import os
 import shutil
 import jax.numpy as np
+import typing
+
+class fixture(typing.Generic[typing.TypeVar("T")]): pass
 
 ROOT: str = "tmp"
 
-def test_is_mask_installed_when_installed() -> None:
-    # Arrange
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
+@pytest.mark.parametrize("root", [ROOT])
+def test_is_mask_installed_when_installed(
+        root: str,
+        remove_installation: fixture[None],
+    ) -> None:
+    """
+    Can mask.is_mask_installed detect and existing installation?
 
-    os.mkdir(ROOT)
-    open(paths.concat([ROOT, "mask.npy"]), "w").close()
+    Fixtures
+    --------
+    remove_installation: fixture[None],
+        Ensures there is no installation before and after the test.
 
-    # Act
-    assert mask.is_mask_installed(ROOT)
-
-    # Clean Up
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
+    Parameters
+    ----------
+    root: str = ROOT
+        Where to look for the installation. Directly parametrises 
+        mask.is_mask_installed. Indirectly parametrizes remove_installation.
+    """
+    open(paths.concat([root, "mask.npy"]), "w").close()
+    assert mask.is_mask_installed(root)
 
 def test_is_mask_installed_when_not_installed() -> None:
     # Arrange
@@ -52,17 +62,6 @@ def test_install_mask_creates_file() -> None:
         shutil.rmtree(ROOT)
 
 def test_install_mask_has_correct_shape() -> None:
-    # Arrange
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
-
-    # Act
     mask.install_mask(ROOT)
-
-    # Assert
     masks: float = np.load(paths.concat([ROOT, "mask.npy"]))
     assert masks.shape == (1024, 1024)
-
-    # Clean Up
-    if os.path.isdir(ROOT):
-        shutil.rmtree(ROOT)
