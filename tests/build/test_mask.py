4902b57724d565cba132a099f16608ed7881e3a6
Jordan Dennis <jdenn105@gmail.com>
2023-02-06 11:14:22 +1000

    Refactoring and documenting `test_install_mask_has_correct_shape` (see #1 and #2).

--- tests/build/test_mask.py
+++ tests/build/test_mask.py
@@ -75,7 +75,25 @@ def test_install_mask_creates_file(
     mask.install_mask(root)
     assert os.path.isfile(paths.concat([root, "mask.npy"]))
 
-def test_install_mask_has_correct_shape() -> None:
-    mask.install_mask(ROOT)
-    masks: float = np.load(paths.concat([ROOT, "mask.npy"]))
+@pytest.mark.parametrize("root", [ROOT])
+def test_install_mask_has_correct_shape(
+        root: str,
+        remove_installation: fixture[None],
+    ) -> None:
+    """
+    Does mask.install_mask reproduce the correct amount of data?
+
+    Fixtures
+    --------
+    remove_installation: fixture[None],
+        Ensures there is no installation before and after the test.
+
+    Parameters
+    ----------
+    root: str = ROOT
+        Where to look for the installation. Directly parametrises 
+        mask.is_mask_installed. Indirectly parametrizes remove_installation.
+    """
+    mask.install_mask(root)
+    masks: float = np.load(paths.concat([root, "mask.npy"]))
     assert masks.shape == (1024, 1024)
