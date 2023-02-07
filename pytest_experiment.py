94159efbcbc8163ca9d7a24977be8ac860255a7c
Jordan Dennis <jdenn105@gmail.com>
2023-02-08 09:49:23 +1000

    Deleting an obselete file.

--- pytest_experiment.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture(scope = "module")
-def im_a_fixture() -> None:
-    return "a"
-
-@pytest.mark.parametrize("a", [im_a_fixture()])
-def test_a(a: str, im_a_fixture) -> None:
-    assert im_a_fixture == "a"
