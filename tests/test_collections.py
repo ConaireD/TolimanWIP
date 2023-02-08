70b5b161cfebe7b8a201f7feaa1c0c84ffa51769
Jordan Dennis <jdenn105@gmail.com>
2023-02-08 10:20:12 +1000

    Implementing `tests/test_collections.py::test_contains_instance_when_type_present`.

---
+++ tests/test_collections.py
@@ -0,0 +1,32 @@
+import toliman.collections as collections
+import pytest
+
+@pytest.mark.parametrize("instance_of,collection", 
+    [
+        (float, [0.0]), 
+        (float, [0, 0.0]),
+        (float, [0.0, 0, "str"]),
+        (float, ["str", 0.0, "str", 0.0, 0]),
+        (str, ["str"]),
+        (str, ["str", 0]),
+        (str, [0, "str", 0.0]),
+        (str, ["str", 0.0, 0, "str"]),
+        (int, [0]),
+        (int, [0, 0]),
+        (int, [0, "str", 0]),
+        (int, ["str", 0.0, 0.0, 0]),
+    ]
+)
+def test_contains_instance_when_type_present(
+        instance_of: type, 
+        collection: list
+    ) -> None:
+    """
+    """
+    assert collections.contains_instance(collection, instance_of)
+
+def test_contains_instance_when_type_is_not_present(
+        instance_of: type,
+    ) -> None:
+    """
+    """
