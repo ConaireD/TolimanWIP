e5da0129f6b24d0054bb0d818bceb815faf2b51a
Jordan Dennis <jdenn105@gmail.com>
2023-02-09 08:49:57 +1000

    Implementing and documenting `tests/test_math.py::test_downsample_aling_axis`.

--- tests/test_math.py
+++ tests/test_math.py
@@ -49,9 +49,7 @@ def test_downsample_square_grid_in_range(
     """
     array: float = jr.normal(jr.PRNGKey(0), (shape, shape), dtype = float)
     resampled: float = math.downsample_square_grid(array, m)
-    array_max: float = array.max()
-    array_min: float = array.min()
-    assert (array_max >= resampled).all() and (array_min <= resampled).all()
+    assert (array.max() >= resampled).all() and (array.min() <= resampled).all()
 
 @pytest.mark.parametrize("shape", [100, 256])
 @pytest.mark.parametrize("m", [1, 2, 5, 10])
@@ -72,6 +70,29 @@ def test_downsample_along_axis_has_correct_shape_when_valid(
     array: float = np.zeros((shape, 2), dtype = float)
     resampled: float = math.downsample_along_axis(array, m, axis = 0)
     assert resampled.shape == (shape // m, 2)
+
+@pytest.mark.parametrize("shape", [100, 256])
+@pytest.mark.parametrize("m", [1, 2, 5, 10])
+def test_downsample_along_axis_in_range(
+        m: int,
+        shape: int,
+    ):
+    """
+    The average should be less than or equal to the largest value and
+    greater than or equal to the smallest value. 
+
+    Parameters
+    ----------
+    m: int
+        The amount to downsample the array by.
+    shape: int
+        The initial shape size of the array.
+    """
+    array: float = jr.normal(jr.PRNGKey(0), (shape, 2), dtype = float)
+    resampled: float = math.downsample_along_axis(array, m)
+    maximums: float = np.max(array, axis = 0)
+    minimums: float = np.min(array, axis = 0)
+    assert (maximums >= resampled).all() and (minimums <= resampled).all()
 
 #def test_photon_noise
 #def test_latent_detector_noise
