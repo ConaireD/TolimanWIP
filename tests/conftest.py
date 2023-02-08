00b92bf1dd974c7a0cf2174f66fef0e6c7f62984
Jordan Dennis <jdenn105@gmail.com>
2023-02-08 11:55:56 +1000

    Refactoring and documenting `tests/conftest.py::make_fake_csv`.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 import os
 import shutil
+import typing
 
-rm: callable = lambda root: shutil.rmtree(root) if os.path.isdir(root) else pass 
-mkdir: callable = lambda root: os.makedirs(root) if not os.path.isdir(root) else pass 
+class fixture(typing.Generic[typing.TypeVar("T")]): pass 
+
+rm: callable = lambda root: shutil.rmtree(root) if os.path.isdir(root) else None 
+mkdir: callable = lambda root: os.makedirs(root) if not os.path.isdir(root) else None
 
 @pytest.fixture
 def rmdir(root: str) -> None:
@@ -24,23 +27,42 @@ def make_fake_csv(
         root: str, 
         number_of_rows: int,
         number_of_columns: int,
+        rmdir: fixture[None],
     ) -> None:
     """
+    Create a phony csv file for testing.
+
+    Fixtures
+    --------
+    rmdir: fixture[None]
+        Ensure files are only extant during tests.
+
+    Parameters
+    ----------
+    root: str
+        The directory to populate with the csv.
+    number_of_rows: int
+        The number of the rows in the csv.
+    number_of_columns: int
+        The number of columns in the csv.
     """
     _: None = mkdir(root)
     file_name: str = "{}/fake.csv".format(root)
     with open(file_name, "w") as file:
-        headers: str = ",".join(
-            "header{}".format(column_header) 
-            for column_header in range(number_of_columns)
+        file.write(
+            os.linesep.join(
+                [
+                    ",".join(
+                        "header{}".format(column_header) 
+                        for column_header in range(number_of_columns)
+                    ),
+                ] + [
+                    ",".join("{}".format(column) 
+                        for column in range(number_of_columns)
+                    )
+                    for row in range(number_of_rows)
+                ]
+            )
         )
-        file.write("{}{}".format(headers, os.linesep))
-        
-        for row in range(number_of_rows):
-            line: str = ",".join(
-                "{}".format(column) 
-                for column in range(number_of_columns)
-            ) + os.linesep
-            file.write(line)
     return file_name            
     
