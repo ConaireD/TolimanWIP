9f86ae60d5f3865801ee29a69bf930415170553c
Jordan Dennis <jdenn105@gmail.com>
2023-02-08 13:28:26 +1000

    Documenting `tests/test_io.py::test_read_csv_to_jax_array_when_file_exists`.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,4 +1,8 @@
 import toliman.io as io
+import pytest
+import typing
+
+class fixture(typing.Generic[typing.TypeVar("T")]): pass
 
 @pytest.mark.parametrize("root", ["tmp", "dat"])
 @pytest.mark.parametrize(
@@ -12,11 +16,25 @@ import toliman.io as io
 def test_read_csv_to_jax_array_when_file_exists(
         number_of_rows: int,
         number_of_columns: int,
-        create_fake_csv: fixture[str],
-        remove_installation: fixture[None],
+        make_fake_csv: fixture[str],
     ) -> None:
     """
+    Fixtures
+    --------
+    make_fake_csv: fixture[str]
+        Cleans the environment, writes a csv and cleans the environment. 
+
+    Parameters
+    ----------
+    number_of_rows: int
+        The number of rows in the csv/array. Indirectly parametrizes 
+        make_fake_csv.
+    number_of_columns: int
+        The number of columns in the csv/array. Indirectly parametrizes 
+        make_fake_csv.
     """
-    array: float = io.read_csv_to_jax_array(create_fake_csv)
-    assert array.shape == (number_of_rows, number_of_columns)
+    array: float = io.read_csv_to_jax_array(make_fake_csv)
+    shape: tuple = (number_of_rows,)
+    if number_of_rows > 0: shape: tuple = shape + (number_of_columns,)
+    assert array.shape == shape
 
