f3520ed5da67a231eb2f149e0e6dba487770ef94
Jordan Dennis <jdenn105@gmail.com>
2023-02-08 10:25:59 +1000

    Implementing `tests/test_collections.py::test_contains_instance_when_type_is_not_present`.

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -25,8 +25,23 @@ def test_contains_instance_when_type_present(
     """
     assert collections.contains_instance(collection, instance_of)
 
+@pytest.mark.parametrize("instance_of,collection",
+    [
+        (float, [0]),
+        (float, [0, "str"]),
+        (float, ["str", 0, 0, "str"]),
+        (str, [0]),
+        (str, [0, 0.0]),
+        (str, [0.0, 0.0, 0, 0]),
+        (int, ["str"],),
+        (int, ["str", 0.0]),
+        (int, [0.0, 0.0, "str", "str"]),
+    ]
+)
 def test_contains_instance_when_type_is_not_present(
         instance_of: type,
+        collection: list,
     ) -> None:
     """
     """
+    assert collections.contains_instance(collections, instance_of)
