9df63ac13187f6ead6dbcb0bb6922de5f62b74cc
Jordan Dennis <jdenn105@gmail.com>
2023-02-14 08:56:06 +1000

    Harvesting the full API and allowing directories as well as files.

--- format-header.py
+++ format-header.py
@@ -13,15 +13,33 @@ def create_header(file_name: str) -> None:
     except StopIteration as stop_iter:
         return
 
-    inline: bool = line.split("[")[-1].strip().isempty()
-    print("inline: [{}]".format(inline))
-
-
+    inline: bool = line.split("[")[-1].strip()
     
+    if len(inline) == 0:
+        api: list = []
+        line: str = next(lines_iter)
+        while not line.strip().startswith("]"):
+            api: list = api + [line.strip().strip(",")]
+            line: str = next(lines_iter)
+    else:
+        api: list = [line.strip() for line in inline.strip("]").split(",")]
 
+    print(api)
+    
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise ValueError("Please provide the name of the file to document.")
 
     file_name: str = sys.argv[1]
-    create_header(file_name)
+
+    if os.path.isdir(file_name):
+        for item in os.walk(file_name):
+            dirname: str = item[0]
+            fnames: list = item[2]
+            files: list = [dirname + "/" + fname for fname in fnames]
+            for file in files:
+                create_header(file)
+    elif os.path.isfile(file_name):
+        create_header(file_name)
+    else:
+        raise ValueError("Please provide a file or directory.")
