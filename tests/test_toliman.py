624913011ce6bb2d811dfbe5cff85f9c5d9c8146
Jordan Dennis <jdenn105@gmail.com>
2023-02-14 13:50:13 +1000

    Documenting `tests/test_toliman.py::TestTolimanOptics`.

--- tests/test_toliman.py
+++ tests/test_toliman.py
@@ -54,43 +54,83 @@ class TestTolimanOptics(object):
     @pytest.mark.xdist_group("2")
     @pytest.mark.software
     def test_constructor_when_static(self: object) -> None:
-        # Arrange/Act
-        static_toliman: object = TolimanOptics(operate_in_static_mode=True)
+        """
+        Does static operation use a static aperture?
 
-        # Assert
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
+        static_toliman: object = TolimanOptics(operate_in_static_mode=True)
         optics: list = static_toliman.to_optics_list()
         assert contains_instance(optics, StaticAperture)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_constructor_when_not_static(self: object) -> None:
-        # Arrange/Act
-        dynamic_toliman: object = TolimanOptics(operate_in_static_mode=False)
+        """
+        Does dynamic operation not include a static aperture.
 
-        # Assert
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
+        dynamic_toliman: object = TolimanOptics(operate_in_static_mode=False)
         optics: list = dynamic_toliman.to_optics_list()
         assert not contains_instance(optics, StaticAperture)
 
     @pytest.mark.xdist_group("2")
     @pytest.mark.software
     def test_constructor_when_mask_too_large(self: object) -> None:
+        """
+        Does a resolution higher than the masks raise an error?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         with pytest.raises(NotImplementedError):
             toliman: object = TolimanOptics(pixels_in_pupil=2048)
 
     @pytest.mark.xdist_group("3")
     @pytest.mark.software
     def test_constructor_when_mask_incorrectly_sampled(self: object) -> None:
-        # Arrange/Act/Assert
+        """
+        If resolution is not a factor of 1024 does it raise an error?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         with pytest.raises(ValueError):
             toliman: object = TolimanOptics(pixels_in_pupil=125)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_constructor_when_mask_is_correct(self: object) -> None:
-        # Arrange/Act
-        toliman: object = TolimanOptics(pixels_in_pupil=256)
+        """
+        Does a possible resolution create an appropriate mask?
 
-        # Assert
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
+        toliman: object = TolimanOptics(pixels_in_pupil=256)
         optics: list = toliman.to_optics_list()
         assert contains_instance(optics, AddOPD)
 
@@ -98,212 +138,327 @@ class TestTolimanOptics(object):
     @pytest.mark.software
     @pytest.mark.skipif(not os.path.isfile(MASK_DIR), reason="The mask is not installed.")
     def test_constructor_when_mask_is_correct_at_max(self: object) -> None:
-        # TODO: Load the mask to get the correct size
+        """
+        Can the program run at the mask resolution?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         mask: float = np.load(MASK_DIR)
         max_size: int = mask.shape[0]
-        # Arrange/Act
         toliman: object = TolimanOptics(pixels_in_pupil=max_size)
-
-        # Assert
         optics: list = toliman.to_optics_list()
         assert contains_instance(optics, AddOPD)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
+    @pytest.mark.skipif(os.path.isfile("i/don't/exist.npy"), reason="File exists.")
     def test_constructor_when_mask_file_is_incorrect(self: object) -> None:
-        # Incorrect file address error.
-        # TODO: Make sure that this file does not exist
-        #       This is a best practice thing as it can theoretically
-        #       exist making this test environment dependent. This
-        #       violates the R in the F.I.R.S.T principles.
+        """
+        Does an incorrect path to the mask raise an error?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         with pytest.raises(ValueError):
             toliman: object = TolimanOptics(path_to_mask="i/don't/exist.npy")
 
     @pytest.mark.xdist_group("4")
     @pytest.mark.software
     def test_constructor_when_aberrated(self: object) -> None:
-        # Arrange/Act
+        """
+        Are aberrations simulated when requested?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics(simulate_aberrations=True)
         optics: list = toliman.to_optics_list()
-
-        # Assert
         assert contains_instance(optics, StaticAberratedAperture)
 
     @pytest.mark.xdist_group("2")
     @pytest.mark.software
     def test_constructor_when_not_aberrated(self: object) -> None:
-        # Arrange/Act
+        """
+        Are aberrations simulated when ignored?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics(simulate_aberrations=False)
         optics: list = list(toliman.layers.values())
-
-        # Assert
         assert not contains_instance(optics, StaticAberratedAperture)
 
     @pytest.mark.xdist_group("3")
     @pytest.mark.software
     def test_constructor_when_polish_is_simulated(self: object) -> None:
-        # Arrange/Act/Assert
+        """
+        Does a request for mirror polish raise an error?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         with pytest.raises(NotImplementedError):
             toliman: object = TolimanOptics(simulate_polish=True)
 
     @pytest.mark.xdist_group("4")
     @pytest.mark.software
     def test_constructor_when_polish_is_not_simulated(self: object) -> None:
-        # Arrange/Act
+        """
+        Is mirror polished ignored when not requested?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics(simulate_polish=False)
         optics: list = toliman.to_optics_list()
-
-        # Assert
         assert not contains_instance(optics, GeometricAberrations)
 
     @pytest.mark.xdist_group("3")
     @pytest.mark.software
     def test_constructor_when_using_fresnel(self: object) -> None:
-        # Operate in Fresnel mode
+        """
+        Does requesting Fresnel raise and error?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         with pytest.raises(NotImplementedError):
             toliman: object = TolimanOptics(operate_in_fresnel_mode=True)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_constructor_when_not_using_fresnel(self: object) -> None:
-        # Arrange/Act
+        """
+        Does not requesting Fresnel succeed?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics(operate_in_fresnel_mode=False)
         optics: list = toliman.to_optics_list()
-
-        # TODO: get the correct name for the FresnelPropagator.
-        # Assert
         assert not contains_instance(optics, FresnelPropagator)
 
     @pytest.mark.xdist_group("4")
     @pytest.mark.software
     def test_insert_when_type_is_incorrect(self: object) -> None:
-        # Arrange
+        """
+        Does insertion of an incorrect type fail?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         with pytest.raises(ValueError):
-            # Act/Assert
             toliman.insert(0, 1)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_insert_when_index_is_too_long(self: object) -> None:
-        # Arrange
+        """
+        Does insertion after the end of the list fail?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         optics: list = toliman.to_optics_list()
         length: int = len(optics)
         wrong_index: int = length + 1
         element_to_insert: object = CircularAperture(1.0)
-
-        # Act/Assert
         with pytest.raises(ValueError):
             toliman.insert(wrong_index, element_to_insert)
 
     @pytest.mark.xdist_group("2")
     @pytest.mark.software
     def test_insert_when_index_is_negative(self: object) -> None:
-        # Arrange
+        """
+        Does insertion using negative indices fail?
+        
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         element_to_insert: object = CircularAperture(1.0)
-
-        # Act/Assert
         with pytest.raises(ValueError):
             toliman.insert(-1, element_to_insert)
 
     @pytest.mark.xdist_group("3")
     @pytest.mark.software
     def test_insert_when_correct(self: object) -> None:
-        # Arrange
+        """
+        Does insert work when the pre-conditions are met?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         optics: list = toliman.to_optics_list()
         length: int = len(optics)
-
-        # Act
         insertion: object = HexagonalAperture(1.0)
         toliman: object = toliman.insert(insertion, 0)
-
-        # Assert
         new_optics: list = toliman.to_optics_list()
         assert contains_instance(new_optics, HexagonalAperture)
 
     @pytest.mark.xdist_group("4")
     @pytest.mark.software
     def test_remove_when_index_is_too_long(self: object) -> None:
-        # Arrange
+        """
+        Is it impossible to remove elements past the end of the list?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         optics: list = toliman.to_optics_list()
         length: int = len(optics)
         wrong_index: int = length + 1
-
-        # Act/Assert
         with pytest.raises(ValueError):
             toliman.remove(wrong_index)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_remove_when_index_is_negative(self: object) -> None:
-        # Arrange
-        toliman: object = TolimanOptics()
+        """
+        Is it possible to remove elements using negative indices?
 
-        # Act/Assert
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
+        toliman: object = TolimanOptics()
         with pytest.raises(ValueError):
             toliman.remove(-1)
 
     @pytest.mark.xdist_group("2")
     @pytest.mark.software
     def test_remove_when_correct(self: object) -> None:
-        # Arrange
+        """
+        Can elements be removed when the pre-conditions are met?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         optics: list = toliman.to_optics_list()
-
-        # Act
         new_toliman: object = toliman.remove(0)
         new_optics: list = new_toliman.to_optics_list()
-
-        # Assert
         assert not contains_instance(new_optics, CreateWavefront)
 
     @pytest.mark.xdist_group("3")
     @pytest.mark.software
-    def test_append_when_type_is_incorrect(self: object) -> None:
-        # Arrange
-        toliman: object = TolimanOptics()
+    @pytest.mark.parametrize("atype", [str, float, int])
+    def test_append_when_type_is_incorrect(self: object, atype: type) -> None:
+        """
+        Is it impossible to append the incorrect type?
 
-        # Act/Assert
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
+        toliman: object = TolimanOptics()
         with pytest.raises(ValueError):
-            toliman.append(1)
+            toliman.append(atype(1))
 
     @pytest.mark.xdist_group("4")
     @pytest.mark.software
     def test_append_when_correct(self: object) -> None:
-        # Arrange
+        """
+        Is it possible to append an element of the correct type?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         element_to_append: object = CircularAperture(1.0)
-
-        # Act
         toliman: object = toliman.append(element_to_append)
         optics: list = toliman.to_optics_list()
-
-        # Assert
         assert contains_instance(optics, CircularAperture)
 
     @pytest.mark.xdist_group("1")
     @pytest.mark.software
     def test_pop_removes_element(self: object) -> None:
-        # Arrange
+        """
+        Does pop remove the last element?
+
+        Marks
+        -----
+        xdist_group: str
+            The named process to run the test on.
+        software: None
+            Tests an implementation detail not physics.
+        """
         toliman: object = TolimanOptics()
         optics: list = toliman.to_optics_list()
-
-        # Act
         new_toliman: object = toliman.pop()
         new_optics: list = new_toliman.to_optics_list()
-
-        # Assert
         assert optics[-1] != new_optics[-1]
-
-#    @pytest.mark.physical
-#    def test_psf_central(self: object) -> None:
-#
-#    @pytest.mark.physical
-#    def test_mask_correct_order_of_magnitude(self: object) -> None:
 
 
 class TestTolimanDetector(object):
